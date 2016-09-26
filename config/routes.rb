4809115603faea91b3af76b8635f2f9566c1c674
09993450 <09993450@aluno.unb.br>
2016-09-26 07:21:44 -0700

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   root :to => redirect('/movies')
+  # Route that posts 'Search TMDb' form
+  post '/movies/search_tmdb'
 end
